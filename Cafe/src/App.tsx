595dad1090a9c96ea973a328007d5f0ec588eb7d
Gabriel Reis <reisgabriel@me.com>
2024-11-20 15:31:59 -0300

    new test

--- Cafe/src/App.tsx
+++ Cafe/src/App.tsx
@@ -1,20 +1,21 @@
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./styles/global.ts";
-import { Router } from "./router.tsx";
-import { BrowserRouter } from "react-router-dom";
-import { defaultTheme } from "./styles/theme/DefaultTheme.tsx";
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+import { CartContextProvider } from './contexts/CartContext'
 
 function App() {
   return (
-    <>
-      <ThemeProvider theme={defaultTheme}>
-        <BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <CartContextProvider>
           <Router />
-        </BrowserRouter>
-        <GlobalStyle />
-      </ThemeProvider>
-    </>
-  );
+        </CartContextProvider>
+      </BrowserRouter>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ Cafe/src/components/Modal/index.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { ModalContainer, ModalContent, XStyled } from "./styles"
+
+interface ModalProps {
+  setIsOpen: (value: boolean) => void;
+  isOpen: boolean
+  children: React.ReactNode
+}
+
+export function Modal({ setIsOpen, isOpen, children }: ModalProps) {
+  return (
+    <>
+      
+      {isOpen && (
+        <ModalContainer>
+          <ModalContent>
+            <XStyled onClick={() => setIsOpen(!isOpen)} />
+            {children}
+          </ModalContent>
+        </ModalContainer>
+      )}
+    </>
+  )
+}
